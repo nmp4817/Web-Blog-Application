3fab8a7dcc2e98c98b422b961b7900d81015ab96
Nabilahmed Patel <nabilpatel11@gmail.com>
2018-02-18 09:39:08 -0600

    Second Commit
    Started design using blade

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+    <div class="col-md-6 px-0">
+      <h1 class="display-4 font-italic">{{$title}}</h1>
+      <p class="lead my-3">A place to informing new readers quickly and efficiently about what's most interesting in this world.</p>
+      <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    </div>
+  </div>
+@endsection
