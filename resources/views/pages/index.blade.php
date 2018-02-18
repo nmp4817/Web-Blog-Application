79e7019de51b4a9d6c07f5adb0af6680317cb99b
Nabilahmed Patel <nabilpatel11@gmail.com>
2018-02-18 15:51:14 -0600

    Third Commit
    Satrted model design
    created db and tables

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
   <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
       <h1 class="display-4 font-italic">{{$title}}</h1>
-      <p class="lead my-3">A place to informing new readers quickly and efficiently about what's most interesting in this world.</p>
+      <p class="lead my-3">A place for informing new readers quickly and efficiently about what's most interesting in this world.</p>
       <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
   </div>
